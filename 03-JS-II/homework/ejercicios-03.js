7d06fe14c3be4903ba12bef7c412eca43866b8c4
emilianobori <emilianobori@gmail.com>
2021-05-05 19:01:57 -0300

    finish homework with a textfile how first time

---
+++ 03-JS-II/homework/ejercicios-03.js
@@ -0,0 +1,10 @@
+1- For: Es un codigo perteneciente a los tipo bucle. Estos tipos de codigo tienen 
+como caracteristica principal que son códigos DRY (dont repeat yourself). Basicamente buscan
+de no tener que repetir operaciones, sino que dando una serie de comandos ya se sepa cuantas 
+veces se tendrian que imprimir ciertos codigos, y tambien bajo que ciertos parametros puestos con anterioridad.
+2- &&: Este es un operador logico (uno de entre otros) que tiene el significado de adhesion, o sea,
+tiene un significado como "y" en español o "and" en ingles.
+3- ||: Es otro operador logico y tiene un significado de opcion, como "o" en español o "or" en ingles
+4- !: Este es el ultimo operador logico que vimos en esta seccion, y es el de negacion, en español seria
+"no" y en ingles "not". Lo que hace es revertir el significado booleano de cualquier codigo, argumento, variable.
+Por ejemplo, si 2 es true, !2 va a ser false.
