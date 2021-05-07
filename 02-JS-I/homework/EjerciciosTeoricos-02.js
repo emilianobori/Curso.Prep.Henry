3e920495431b5c780d30b6d1f940f52ab7bc1202
emilianobori <emilianobori@gmail.com>
2021-05-06 22:35:40 -0300

    js-3 exercises and teoric homework js-1

---
+++ 02-JS-I/homework/EjerciciosTeoricos-02.js
@@ -0,0 +1,17 @@
+Variables: Es el medio por el cual un concepto de la interfaz puede adoptar un valor. Hay distintos tipos 
+de variables y de valores. Tipos de variables: var, let, const. Tipos de valores: Strings, numers, booleans, 
+null, nan, undefined.
+Strings: Son un tipo de valor que puede ser asignado a una variable especifica. Practicamente, son un conjunto 
+de caracteres que deben ser escritos entre comillas para que adopten dicha tipicidad.
+Funciones: Son un conjunto de codigos que le indican a la interfaz como se relacionan entre si y con otras 
+variables, dando como output un resultado. Argumentos: Los argumentos van entre los parentesis de cualquier 
+funcion e indica que es lo que se está pidiendo o demandando como entrada para que la funcion devuelva un 
+resultado. Return: Es una variable dentro de la funcion que precisa lo que tiene que devolver como resultado 
+dicha funcion. Si el return indica diferencia, lo que se esta pidiendo es que el resultado sea la resta de dos 
+numeros que haya pedido con anterioridad el argumento.
+Declaraciones: If: Es muy parecido a la funcion SI de Excel, esta indicando que ante cierta condicion que esté 
+ocurriendo en la funcion, devuelva un valor booleano (no estoy seguro si puede devolver otro tipo de valor)
+Valores Booleanos: Estos tipos de valores son a raiz de una respuesta binaria, "si o no", "blanco o negro"; 
+pero para representar este tipo de respuestas se dan como resultado "true o false". De esta manera, si se pide 
+que el imput ingresado sea mayor o menor que cierto parametro, la respuesta sea o una u otra, o "true o false"
+
