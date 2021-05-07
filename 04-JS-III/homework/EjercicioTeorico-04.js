43513bfc8e9d882c934577ddabfbc3504344b5c8
emilianobori <emilianobori@gmail.com>
2021-05-07 20:17:42 -0300

    understanding about 75%

---
+++ 04-JS-III/homework/EjercicioTeorico-04.js
@@ -0,0 +1,4 @@
+Arrays: Traducido al español significa 'arreglos'. Basicamente es un elemento, no un elemento nativo como strings
+o numbers, pero que puede tener otros elementos dentro. Este conjunto de elementos tiene a los mismos de una manera
+ordenada, por lo cual tambien se lo puede llamar lista. A su vez, por medio de algunas funciones (pop y push), se 
+pueden quitar o agregar elementos respectivamente; y tambien hacer algunas operaciones con estos arreglos.
