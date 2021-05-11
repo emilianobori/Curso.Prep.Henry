98c552da9407ababa8d8db01286d3b44da7c093a
emilianobori <emilianobori@gmail.com>
2021-05-11 20:53:33 -0300

    exercise 3, agregarStringInvertida, working with numbers only

---
+++ 06-JS-V/homework/EjercicioTeorico-06.js
@@ -0,0 +1,13 @@
+1- `prototype`: Los prototipos en javascript son una suerte de matrices de ciertos objetos con los que 
+comparten propiedades. Por ejemplo: si creamos un objeto llamado Personas {}, que tenga determinadas propiedades
+(como 'nombre', 'apellido'), éste va a ser prototipo de otro que tal vez se llame Estudiantes {}, que tenga las
+mismas propiedades ('nombre' y 'apellido'), pero tambien puede tener otras mas especificas de ese objeto (como 
+	'curso' , 'calificaciones'). Este ultimo (Estudiantes) va a ser una clase del anterior (Personas).
+2- _Constructors_ (de Clases): Las funciones de tipo construction se utilizan para generar objetos de manera mas
+eficiente dentro de un objeto prototipo de estos nuevos.
+
+
+
+
+
+
