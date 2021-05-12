918ff326aa579ff234c9ea33fbded4c1562e08b8
emilianobori <emilianobori@gmail.com>
2021-05-12 17:56:20 -0300

    Callbacks

---
+++ 07-JS-VI/homework/EjercicioTeorico-07.js
@@ -0,0 +1,8 @@
+Funciones Callback: Es como se denominan a las funciones dentro de otras funciones. Podria decirse que es 
+una forma, entre otras, de escribir funciones para la realizacion de un codigo. En este caso, cuando utilizamos
+callbacks, se le llama 'modo de programacion DECLARATIVO'. Algunas de las funciones que se utilizan en esta 
+modalidad: 
+-for.Each
+-reduce
+-map
+ 
